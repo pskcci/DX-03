37764427e615c444911ef18a778fae1459624a89
gyeongjun-web <wnsdign@naver.com>
2025-07-15 16:16:14 +0900

    assert modify

--- class00/python/Test.py
+++ class00/python/Test.py
@@ -1,18 +1,21 @@
 #정경준
 
 import random
-sentence=[["나는","학교를","간다."],["나는","운동을","간다."],["나는","집으로","간다"],["나는","밥을","먹는다."],["나는","밥을","먹는다."]]
+sentence=[["나는","학교를","간다"],["나는","운동을","간다"],["나는","집으로","간다"],["나는","밥을","먹는다"],["나는","밥을","먹는다"]]
 print("다음 단어들을 순서대로 조합하세요:")
 score=0
 
 
 for i in range(5):
     chooselist = random.choice(sentence)
-    random.shuffle(chooselist)
-    print(f'제시어:{chooselist[0]} {chooselist[1]} {chooselist[2]}')
+    answer_right = chooselist[0] + " "  + chooselist[1] + " " + chooselist[2]
+    quizSen = chooselist[:]
+    random.shuffle(quizSen)
+    print(f'제시어:{quizSen[0]} {quizSen[1]} {quizSen[2]}')
     print("정확한 문장을 입력하세요:")
     user_input=input('입력:')
-    if user_input.strip()==sentence[i]:
+
+    if user_input == answer_right:
         score+=1
         print('정답:')
     else:
